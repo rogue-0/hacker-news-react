e708b54509936cfc24dae819aa0c33b9ac93158c
Clover <cloverts@proton.me>
2024-04-28 19:17:12 +0500

    add newsfeed refresh logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-import { PanelHeader, Group, Spinner, CardGrid } from "@vkontakte/vkui";
+import { PanelHeader, Group, Spinner, CardGrid, Button } from "@vkontakte/vkui";
 
 import Article from "../entities/Article/Article";
 
@@ -14,7 +14,35 @@ export default function Home() {
 
       try {
         const res = await fetch(
-          "https://hacker-news.firebaseio.com/v0/topstories.json"
+          "https://hacker-news.firebaseio.com/v0/newstories.json"
+        );
+
+        const ids = await res.json();
+
+        setIds(ids.slice(0, 100));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchIds();
+
+    const interval = setInterval(() => {
+      fetchIds();
+    }, 60000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const refreshNewsfeed = useCallback(() => {
+    async function fetchIds() {
+      setIsLoading(true);
+
+      try {
+        const res = await fetch(
+          "https://hacker-news.firebaseio.com/v0/newstories.json"
         );
 
         const ids = await res.json();
@@ -33,17 +61,27 @@ export default function Home() {
   return (
     <>
       <PanelHeader>Hacker News VK</PanelHeader>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <Group style={{ width: "800px" }}>
+      <Group mode="plain" style={{ width: "800px" }}>
+        <Button
+          size="m"
+          stretched
+          onClick={refreshNewsfeed}
+          disabled={isLoading}
+        >
+          {isLoading ? <Spinner /> : "Refresh comments"}
+        </Button>
+      </Group>
+      <Group style={{ width: "800px" }}>
+        {isLoading ? (
+          <Spinner />
+        ) : (
           <CardGrid size="l">
             {ids.map((id) => {
               return <Article key={id} id={id} />;
             })}
           </CardGrid>
-        </Group>
-      )}
+        )}
+      </Group>
     </>
   );
 }
